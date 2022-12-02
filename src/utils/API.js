dbfaf5b8bc44371bca987e94103c68972d15c1d4
Joe Rehfuss <jrehfuss@c02g81atq05q.domain>
2022-12-02 11:21:33 -0800

    updates

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,7 @@
 // in dev mode
-const URL_PREFIX= "http://localhost:3001"
+// const URL_PREFIX= "http://localhost:3001"
 // in prod mode
-// const URL_PREFIX= "http://joesbacktodos.herokapp.com"
+const URL_PREFIX= "https://todo-joewt.herokuapp.com"
 
 const  API = {
     login: (userObj)=>{
